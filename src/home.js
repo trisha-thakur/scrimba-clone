5b4c32a1b9ed7e61903c2bb6373ab64477fbb3d2
trisha-thakur <trishathakur304@gmail.com>
2022-11-10 15:32:43 +0530

    add faq grid

--- src/home.js
+++ src/home.js
@@ -117,6 +117,63 @@ const Home = () => {
         <span className="to-the"> to the </span>
         <span className="faq"> Q</span>
       </div>
+      <div className="content8">
+        <div className="grid-container">
+          <div className="gird-element">
+            <p className="grid-title">Is Scrimba a coding bootcamp?</p>
+            <p className="grid-content">
+              In short, no we are not. We aim to offer the same value, but at
+              100X lower price, so there are differences. Scrimba allows you to
+              study where you want, when you want. We also substitute the
+              classroom teaching experience with interactive screencast, live
+              events, and peer-to-peer help between students.
+            </p>
+          </div>
+          <div className="gird-element">
+            <p className="grid-title">Do I get in-person help from teachers?</p>
+            <p className="grid-content">
+              Getting help at Scrimba is done peer-to-peer, meaning students
+              help and support each other, instead of teacher-to-student. This
+              is an important ingredient in being able to charge 1% of what
+              coding bootcamps charge.
+            </p>
+          </div>
+          <div className="gird-element">
+            <p className="grid-title">When do the courses start?</p>
+            <p className="grid-content">
+              Whenever you want. Our courses are all self-paced, meaning you
+              follow your own schedule. Sign up and get started today if you
+              would like!
+            </p>
+          </div>
+          <div className="gird-element">
+            <p className="grid-title">Do you have team plans?</p>
+            <p className="grid-content">
+              We sure do! Complete The Team Plans Form and our team will get
+              back to you within 48 hours.
+            </p>
+          </div>
+          <div className="gird-element">
+            <p className="grid-title">How long does the career path take?</p>
+            <p className="grid-content">
+              You can complete the career path in three months if you are fast.
+              But it will realistically take you 6-12 months to reach a hireable
+              level as a frontend developer.
+            </p>
+          </div>
+          <div className="gird-element">
+            <p className="grid-title">I have more questions. Who can I ask?</p>
+            <p className="grid-content">
+              Please join our Discord server and ask in the #general channel.
+              There, you will be replies from both the community and Team
+              Scrimba.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="content9">
+        <button className="btn3">Visit Path</button>
+      </div>
     </div>
   );
 };
